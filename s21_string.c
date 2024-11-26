65e13b17fffb6c29ea3d2c33b43ef079df96bfad
Gibbous Jannette <gibbousj@ox-v5.msk.21-school.ru>
2024-11-26 19:54:06 +0300

    string

--- s21_string.c
+++ s21_string.c
@@ -5,19 +5,20 @@
 void *s21_memchr(const void *str, int c, size_t n);
 int s21_memcmp(const void *str1, const void *str2, size_t n);
 void *s21_memcpy(void *dest, const void *src, size_t n);
+void *s21_memset(void *str, int c, size_t n);
+char *s21_strncat(char *dest, const char *src, size_t n);
+char *s21_strchr(const char *str, int c);
+int s21_strncmp(const char *str1, const char *str2, size_t n);
+char *s21_strncpy(char *dest, const char *src, size_t n);
 
 int main() {
-   // Исходный массив данных
-   unsigned char src[10]="1234567890";
 
-   // Вывод массива src на консоль
-   printf ("src old: %s\n",src);
+    char str1[15] = "catpopa";
+    char str2[30] = "catpopa";
 
-   // Копируем 3 байт
-   memmove (&src[4], &src[3], 3);
+    int rez = s21_strncmp(str1, str2, 20);
 
-   // Вывод массива src на консоль
-   printf ("src new: %s\n", src);
+    printf("%d - %d", strncmp(str1, str2, 20), s21_strncmp(str1, str2, 20));
 
    return 0;
 }
@@ -49,6 +50,49 @@ int s21_memcmp(const void *str1, const void *str2, size_t n) {
 void *s21_memcpy(void *dest, const void *src, size_t n) {
     for(int i = 0; i < n; i++) {
         *(unsigned char*)(dest + i) = *(unsigned char*)(src + i);
+    }
+    return dest;
+}
+
+void *s21_memset(void *str, int c, size_t n) {
+    for(int i = 0; i < n; i++) {
+        *(unsigned char*)(str + i) = c;
+    }
+    return str;
+}
+
+char *s21_strncat(char *dest, const char *src, size_t n) {
+    while(*dest != '\0') dest++;
+    int i = 0;
+    for(; i < n && src[i] != '\0'; i++) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+    return dest;
+}
+
+char *s21_strchr(const char *str, int c) {
+    char *res = NULL;
+    while(*str != '\0' && res == NULL) {
+        if(c == *str)
+            res = (char*)str;
+        str++;
+    }
+    return res;
+}
+
+int s21_strncmp(const char *str1, const char *str2, size_t n) {
+    int rez = 0;
+    for(int i = 0; i < n && rez == 0 && *str1 != '\0' && *str2 != '\0'; i++) {
+        rez = *(str1++) - *(str2++);
+    }
+
+    return rez;
+}
+
+char *s21_strncpy(char *dest, const char *src, size_t n) {
+    for(int i = 0; i < n && *src != '\0'; i++) {
+        *(dest++) = *(src++);
     }
     return dest;
 }
