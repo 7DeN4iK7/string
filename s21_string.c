df55c79eb508254b63917a88d6fb044a3c3da65d
Gibbous Jannette <gibbousj@ox-v5.msk.21-school.ru>
2024-11-26 16:07:32 +0300

    memcmp, memcpy

--- s21_string.c
+++ s21_string.c
@@ -4,15 +4,20 @@
 
 void *s21_memchr(const void *str, int c, size_t n);
 int s21_memcmp(const void *str1, const void *str2, size_t n);
+void *s21_memcpy(void *dest, const void *src, size_t n);
 
 int main() {
-   unsigned char src[15]="1234567899";
-   unsigned char dst[15]="1234567891";
+   // Исходный массив данных
+   unsigned char src[10]="1234567890";
 
-   // Сравниваем первые 10 байт массивов
-   // и результат выводим на экран
-   int res = memcmp (src, dst, 10);
-   printf("%d", res);
+   // Вывод массива src на консоль
+   printf ("src old: %s\n",src);
+
+   // Копируем 3 байт
+   memmove (&src[4], &src[3], 3);
+
+   // Вывод массива src на консоль
+   printf ("src new: %s\n", src);
 
    return 0;
 }
@@ -34,7 +39,16 @@ void *s21_memchr(const void *str, int c, size_t n) {
 
 int s21_memcmp(const void *str1, const void *str2, size_t n) {
     int rez = 0;
+    for(int i = 0; i < n && rez == 0; i++) {
+        rez = *(unsigned char*)(str1 + i) - *(unsigned char*)(str2 + i);
+    }
 
     return rez;
 }
 
+void *s21_memcpy(void *dest, const void *src, size_t n) {
+    for(int i = 0; i < n; i++) {
+        *(unsigned char*)(dest + i) = *(unsigned char*)(src + i);
+    }
+    return dest;
+}
